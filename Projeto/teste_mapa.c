e81c5e50067573b6a81876cf47dcefd3a7810d4d
Xiro <andredrez03@gmail.com>
2023-05-06 18:03:54 +0100

    Update teste_mapa.c

--- Projeto/teste_mapa.c
+++ Projeto/teste_mapa.c
@@ -9,26 +9,30 @@
 
 int preenche_atoa() 
 {
-
-    int x = 20;
-    int y = 0;
-
-    // numero aleatorio de 40 a 60, para definir a probabilidade de hastags no mapa
+    // numero aleatoreo.
     srand(time(NULL));
-    int numero_aleatorio = rand() % 20 + 40;
-
-    // preencher na matriz os hastags consoante a probabilidade
-    gera_mapa(x,y,numero_aleatorio);
+    int numero_aleatorio = rand() % 100;
 }
 
-void gera_mapa(x,y,random)
+void gera_mapa(matriz[int x,int y])
 {
-    int area = x * y;
-    int area_ocupada = area * (random/100);
+    int rowinicial = 0; //temporario ns as cenas do hud
+    int colinicial = 0; //mm cena
 
-    for (i=0; i<area_ocupada; i++)
+    for (int ys=rowinicial; ys<y; ys++)
     {
-        
+        for(int xs=colinicial; xs<x; xs++)
+        {
+            int chance = randomgen(); //sempre (0<=chance<100)
+            if (chance<45)
+            {
+                matriz[ys,xs].charact = '#'; //45%de chance de os blocos serem # (45/100 blocos sao #)
+            } 
+            else
+            {
+                matriz[ys,xs].charact = '.'; //55%  
+            }
+        }
     }
 }
 
@@ -42,14 +46,23 @@ void gera_mapa(x,y,random)
 
 
 //denoise
-void conta_vizinhos()
+int conta_vizinhos()
 {
+    int vizinhos=0;
 
+
+
+
+
+
+
+    return vizinhos;
 }
 
-void espeta_Hastag ()
+void espeta_Hastag (int vizinhos, xs, ys)
 {
-    if(vizinhos == 0) faz_parede;
-    if(vizinhos > 4) faz_vazio;
-    if(vizinhos > 5) faz_parede;
+    if(vizinhos == 0) // faz_parede (xs,ys);
+
+    if(vizinhos > 4) // faz_vazio (xs,ys);
+    if(vizinhos > 5) // faz_parede (xs,ys);
 }
