b946cb16a9c8fda96936e71a0cfb4c38c9afdfa1
Lothean0 <xxsimaoxx1@gmail.com>
2023-05-09 10:23:01 +0100

    .

--- Projeto/movimento.c
+++ Projeto/movimento.c
@@ -6,70 +6,100 @@ typedef struct vetor
 {
     int coorY;
     int coorX;
-}Vetor;
+} Vetor;
 
-//func que define um vetor baseado na tecla premida
+// func que define um vetor baseado na tecla premida
 Vetor calcvetor(void)
 {
-    //nao imprimir na tela o input
-    //ativar o keypad para podermos tambem usar as setas
+    // nao imprimir na tela o input
+    // ativar o keypad para podermos tambem usar as setas
     noecho();
-    keypad(stdscr,true);
+    keypad(stdscr, true);
 
-    //define um vetor
+    // define um vetor
     Vetor direcao;
-    direcao.coorX=0;
-    direcao.coorY=0;
+    direcao.coorX = 0;
+    direcao.coorY = 0;
 
-    //input da tecla
-    int tecla=getch();
+    // input da tecla
+    int tecla = getch();
 
-    switch(tecla) {
-        case KEY_A1:
-		case '7': direcao.coorX = -1; direcao.coorY = -1; break;
-        case KEY_UP:
-		case '8': direcao.coorX = +0; direcao.coorY = -1; break;
-        case KEY_A3:
-		case '9': direcao.coorX = +1; direcao.coorY = -1; break;
-        case KEY_LEFT:
-		case '4': direcao.coorX = -1; direcao.coorY = +0; break;
-        case KEY_B2:
-		case '5': break;
-        case KEY_RIGHT:
-		case '6': direcao.coorX = +1; direcao.coorY = +0; break;
-        case KEY_C1:
-		case '1': direcao.coorX = -1; direcao.coorY = +1; break;
-        case KEY_DOWN:
-		case '2': direcao.coorX = +0; direcao.coorY = +1; break;
-        case KEY_C3:
-		case '3': direcao.coorX = +1; direcao.coorY = +1; break;
-        case 'q': endwin(),exit(0);
-	}
-    return direcao; 
+    switch (tecla)
+    {
+    case KEY_A1:
+    case '7':
+        direcao.coorX = -1;
+        direcao.coorY = -1;
+        break;
+    case KEY_UP:
+    case '8':
+        direcao.coorX = +0;
+        direcao.coorY = -1;
+        break;
+    case KEY_A3:
+    case '9':
+        direcao.coorX = +1;
+        direcao.coorY = -1;
+        break;
+    case KEY_LEFT:
+    case '4':
+        direcao.coorX = -1;
+        direcao.coorY = +0;
+        break;
+    case KEY_B2:
+    case '5':
+        break;
+    case KEY_RIGHT:
+    case '6':
+        direcao.coorX = +1;
+        direcao.coorY = +0;
+        break;
+    case KEY_C1:
+    case '1':
+        direcao.coorX = -1;
+        direcao.coorY = +1;
+        break;
+    case KEY_DOWN:
+    case '2':
+        direcao.coorX = +0;
+        direcao.coorY = +1;
+        break;
+    case KEY_C3:
+    case '3':
+        direcao.coorX = +1;
+        direcao.coorY = +1;
+        break;
+    case 'q':
+        endwin(), exit(0);
+    }
+    return direcao;
 }
 
-//func que soma o vetor de ''calcvetor'' à posiçao do player depois de verificar se esta é valida
+// func que soma o vetor de ''calcvetor'' à posiçao do player depois de verificar se esta é valida
 void mudarstate(Player *jogador)
 {
-    
-    //começa por pegar um vetor(input da tecla, etc...)
+
+    // começa por pegar um vetor(input da tecla, etc...)
     Vetor direcao = calcvetor();
 
-    //define pTEMP como o "bloco" para onde o player quer ir
-    Player pTEMP = {0,0};
-    pTEMP.coorX=(jogador->coorX)+(direcao.coorX);
-    pTEMP.coorY=(jogador->coorY)+(direcao.coorY);
+    // define pTEMP como o "bloco" para onde o player quer ir
+    Player pTEMP = {0, 0};
+    pTEMP.coorX = (jogador->coorX) + (direcao.coorX);
+    pTEMP.coorY = (jogador->coorY) + (direcao.coorY);
 
-    //mvinch como o char para onde o player quer ir. Isto e a condiçao para verificar e o movimento e possivel ou nao
-    
-    switch(mvinch(pTEMP.coorY, pTEMP.coorX)) {
-        //casos em que queremos ir para uma parede ou algum local cujo nao podemos ir
-        case '#': break; // nestes casos o player nao mexe;
-        default: 
-            //Fazer uma trail dos movimentos do jogador, ou seja, adicionar um . as posiçoes previas
-            mvaddch(jogador->coorY, jogador->coorX, '.'); 
-            jogador->coorX=pTEMP.coorX; jogador->coorY=pTEMP.coorY; 
-            break;//neste caso a posiçao do player passa a ser a do pTEMP
+    // mvinch como o char para onde o player quer ir. Isto e a condiçao para verificar e o movimento e possivel ou nao
+
+    switch (mvinch(pTEMP.coorY, pTEMP.coorX))
+    {
+    // casos em que queremos ir para uma parede ou algum local cujo nao podemos ir
+    case '#':
+        break; // nestes casos o player nao mexe;
+    default:
+        // Fazer uma trail dos movimentos do jogador, ou seja, adicionar um . as posiçoes previas
+        mvaddch(jogador->coorY, jogador->coorX, '.');
+        jogador->coorX = pTEMP.coorX;
+        jogador->coorY = pTEMP.coorY;
+        break; // neste caso a posiçao do player passa a ser a do pTEMP
     }
     mvaddch(jogador->coorY, jogador->coorX, '@' | A_BOLD);
 }
