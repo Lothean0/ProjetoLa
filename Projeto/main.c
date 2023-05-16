3b258946a9db272f6fcfb537f126f407e5470037
Matias Gabriel (Gonçalves)² <tobyfox004@gmail.com>
2023-05-16 10:08:23 +0100

    Update main.c

--- Projeto/main.c
+++ Projeto/main.c
@@ -55,114 +55,12 @@ int main(void)
     mvvline(0, HudX, '#', MaxY);      // linha da esquerda
     mvvline(0, MaxHudX, '#', MaxY);   // linha da direita
 
-    // gerar mapa?
+    // geracao de mapa
     Mapa mapa[MaxY][MaxX];
 
     gera_mapa(MaxY,MaxX, mapa);
-    /*int i, j, seed;
-    seed = (time(NULL));
-    for (i = 0; i < MaxY; i++)
-    {
-        for (j = 0; j < MaxX; j++)
-        {
-            int chance = randomgen(seed); // sempre (0<=chance<100)
-            if (chance < 46)
-            {
-                mapa[i][j].character = '#';
-                mapa[i][j].distancia = 0;
-                mapa[i][j].cor = COLOR_PAIR(Nao_Visivel);
-                mapa[i][j].visao = 0;
-            }
-            else
-            {
-                mapa[i][j].character = '.'; // 55%
-                mapa[i][j].distancia = 0;
-                mapa[i][j].cor = COLOR_PAIR(Nao_Visivel);
-                mapa[i][j].visao = 0;
-            }
-            seed -= 42;
-
-            if (i == 0 || j == 0 || i == (MaxY - 1) || j == (MaxX - 1) || i == 1 || j == 1)
-            {
-                mapa[i][j].character = '#';
-            }
-        }
-    }*/
     denoiser(MaxY,MaxX, mapa);
-    //  DENOISER
-    /*int maxreps = 7;
-    for (int reps = 0; reps < maxreps; reps++)
-    {
-        for (int ys = 2; ys < MaxY - 1; ys++)
-        {
-            for (int xs = 2; xs < MaxX - 1; xs++)
-            {
-                // Contador
-                int vizinhos = 0;
-
-                if ((ys > 1 && ys < (MaxY - 1)) && (xs > 1 && xs < (MaxX - 1)))
-                {
-                    // verifica quantas das posiçoes à volta da y,x sao #'s
-                    if ((mapa[ys + 1][xs].character) == '#')
-                    {
-                        vizinhos++;
-                    }
-                    if ((mapa[ys - 1][xs].character) == '#')
-                    {
-                        vizinhos++;
-                    }
-                    if ((mapa[ys][xs + 1].character) == '#')
-                    {
-                        vizinhos++;
-                    }
-                    if ((mapa[ys][xs - 1].character) == '#')
-                    {
-                        vizinhos++;
-                    }
-                    if ((mapa[ys + 1][xs + 1].character) == '#')
-                    {
-                        vizinhos++;
-                    }
-                    if ((mapa[ys - 1][xs - 1].character) == '#')
-                    {
-                        vizinhos++;
-                    }
-                    if ((mapa[ys + 1][xs - 1].character) == '#')
-                    {
-                        vizinhos++;
-                    }
-                    if ((mapa[ys - 1][xs + 1].character) == '#')
-                    {
-                        vizinhos++;
-                    }
-                }
-
-                // TIPO MINESWEEPER
-                if (vizinhos == 0 || vizinhos > 4) // faz_parede (xs,ys);
-                {
-                    mapa[ys][xs].character = '#';
-                }
-                else if (vizinhos < 4) // faz_vazio (xs,ys);
-                {
-                    mapa[ys][xs].character = '.';
-                }
-
-                // Na ultima repetiçao esta parte limpa os #'s inuteis no meio do mapa
-                if (reps == maxreps - 1)
-                {
-                    if (vizinhos == 0) // faz_parede (xs,ys);
-                    {
-                        mapa[ys][xs].character = '.';
-                    }
-                    else if (vizinhos == 8)
-                    {
-                        mapa[ys][xs].character = '#';
-                    }
-                }
-            }
-        }
-    }*/
-
+    
     // PRINT
     for (int ys = 0; ys < MaxY; ys++)
     {
