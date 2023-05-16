4bbf93eb4b355e342f13bc4d7adad59e18ef124f
Josefin0 <pedrooliv345@gmail.com>
2023-05-16 18:30:40 +0100

    azul
    e branco e o coraçao

--- Projeto/iluminacao.c
+++ Projeto/iluminacao.c
@@ -15,7 +15,7 @@ void inicializar_cor(void)
     start_color();
     init_pair(Visivel, COLOR_WHITE, COLOR_BLACK);
     init_pair(Nao_Visivel, COLOR_BLACK, COLOR_BLACK);
-    init_pair(3,COLOR_CYAN, COLOR_BLACK);
+    init_pair(3,COLOR_BLUE, COLOR_BLACK);
 }
 
 void colorir(Player *jogador)
